c888b31340c7554c39cf6d9c91a8e007b4c0c3d1
Toshiya MORI <mori@kurusugawa.jp>
2018-10-18 02:44:58 +0900

    rename dotty010 files and add LiteralSingletonType

---
+++ dotty010/src/main/scala/jp/seraphr/sandbox/dotty/ref01_types00_LiteralSingletonTypes.scala
@@ -0,0 +1,19 @@
+package jp.seraphr.sandbox.dotty
+
+// 何故かドキュメントの目次からリンクが消えていた・・・
+// http://dotty.epfl.ch/docs/reference/singleton-types.html
+object LiteralSingletonTypes {
+  val t: 42 = 42
+  val x: "Jedi" = "Jedi"
+
+  trait Mat[N <: Singleton, M <: Singleton] {
+    def add(aThat: Mat[N, M]): Mat[N, M]
+  }
+
+  val m1: Mat[2, 3] = ???
+  val m2: Mat[2, 3] = ???
+  val m3: Mat[4, 3] = ???
+
+  m1.add(m2)
+  // m1.add(m3) // コンパイルエラー
+}
